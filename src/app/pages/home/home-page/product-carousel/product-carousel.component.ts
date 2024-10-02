1684d5afa06f4acfa36fc869a8ea07d0dd0656c6
abdelrahman hossam <1abdelrahmanhossam@gmail.com>
2024-10-02 15:40:04 +0300

    footer added

--- src/app/pages/home/home-page/product-carousel/product-carousel.component.ts
+++ src/app/pages/home/home-page/product-carousel/product-carousel.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CardComponent } from "../../../../shared/components/card/card.component";
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-carousel',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent ,RouterLink, CommonModule ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './product-carousel.component.html',
   styleUrl: './product-carousel.component.css'
 })
 export class ProductCarouselComponent {
+  allProducts: any[] = []; // All products fetched from the API
+  products: any[] = []; // Filtered products to be displayed in the cards
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  // Fetch all products from the API
+  fetchProducts() {
+    this.http.get<any>('https://dummyjson.com/c/3d86-7e5a-443d-b6fc') // Replace with your actual API
+      .subscribe((data) => {
+        console.log('Fetched data:', data); // Log the fetched data
+        this.allProducts = data; // Assuming 'products' is the key in the API response
+        this.products = this.allProducts; // Initially display all products
+      }, error => {
+        console.error('Error fetching products:', error);
+      });
+  }
 }
